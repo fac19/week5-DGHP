17f7092e913c5dcf75765b59a87a8a673e221ee2
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-04-01 16:40:48 +0100

    populate with fake data Relates #6
    Co-authored-by: vatskan <vatskan@gmail.com>
    Co-authored-by: Ivo-Evans <ievans.storrie@gmail.com>
    Co-authored-by: tacotoemeck <wagner.tomek@gmail.com>

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -14,5 +14,12 @@ CREATE TABLE blog_posts (
     post_date DATE NOT NULL DEFAULT CURRENT_DATE 
 );
 
+INSERT INTO users (username) VALUES ('tom'), ('ako'), ('Ivo'), ('Vats');
+
+INSERT INTO blog_posts (author_id, post) VALUES 
+(1, 'great stuff'),
+(2, 'very good stuff'),
+(3, 'we are gonna smash it'),
+(4, 'Wait, i am not ready');
 
 COMMIT;
