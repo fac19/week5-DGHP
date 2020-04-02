a1b992740531ffd976cf3b32762c5c0e8b638051
akomiqaia <akaki.mikaia.1@iliauni.edu.ge>
2020-04-02 12:04:45 +0100

    write submit post handler for POST method Relates #10
    Co-authored-by: Ivo-Evans <ievans.storrie@gmail.com>

--- src/handlers/submitPost.js
+++ src/handlers/submitPost.js
@@ -5,6 +5,7 @@ function submitPostHandler(req, res) {
   let body = ""
   req.on('data', chunk => body += chunk)
     .catch(err => console.error(err))
+
   req.on('end', () => {
     const message = new URLSearchParams(body);
     const messageObject = Object.fromEntries(message)
