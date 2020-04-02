3a2865c23e8a6b05fb2d1537043f65efb1e75d65
tacotoemeck <wagner.tomek@gmail.com>
2020-04-02 12:08:00 +0100

    Add homeHandler with template Relates #5, #10 Co-authored Co-authored-by: vatskan <vatskan@gmail.com>

--- src/handlers/home.js
+++ src/handlers/home.js
@@ -1,3 +1,16 @@
 const templates = require("../template");
+const db = require("../db/connection.js");
 
-console.log(templates[5]);
+function homeHandler(request, response) {
+  db.query("SELECT * FROM users")
+    .then(result => result.rows)
+    .then(users => {
+      const userList = users.map(user => `<li>${user.username}</li>`);
+      response.writeHead(200, { "content-type": "text/html" });
+      response.end(`<ul>${userList.join("")}</ul>`);
+    });
+  //   response.writeHead(200, { "content-type": "text/html" });
+  //   response.end(`<h1>Hello</h1>`);
+}
+
+module.exports = homeHandler;
--- src/template.js
+++ src/template.js
@@ -1,1 +1,37 @@
-module.exports = { 5: 5 };
+function sharedLayout(bodyContent) {
+  return `<!DOCTYPE html>
+    <html lang="en">    
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="description" content="Pink Punk">
+            <link rel="stylesheet" href="public/main.css">
+            <title>Pink Punk</title>
+        </head>
+        <body>
+            ${bodyContent}
+        </body>
+        <script src="public/main.js"></script>
+    </html>
+    `;
+}
+
+function home() {
+  return sharedLayout(`    
+  <div class="container">
+  <section>
+    <article class="post">
+      <p class="post__author">By: ${name}</p>
+      <p class="post__date">When: ${date}</p>
+      <p class="post__textContent">${textBox}</p>
+      <button
+        class="post__remove-button"
+        aria-label="button to remove post"
+        type="button"
+      >
+        Remove Post
+      </button>
+    </article>
+  </section>
+</div>`);
+}
