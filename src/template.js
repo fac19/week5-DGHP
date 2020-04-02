d8eba144be1d8ef4987b01bcf679e5175d0ba440
Ivo Evans <ievans.storrie@gmail.com>
2020-04-02 11:23:22 +0100

    Add submitPage template function Relates #5
    Co-authored-by: akomiqaia <akaki.mikaia.1@iliauni.edu.ge>

--- src/template.js
+++ src/template.js
@@ -1,1 +1,15 @@
-module.exports = { 5: 5 };
+function submitPage(params) {
+  return sharedContent(
+    `
+    <form action="submit" method="POST">
+      <label for="username">Username</label>
+      <input id="username" name="username" placeholder="who are you?" required>
+      <label for="post_text">Write Post</label>
+      <textarea id="post_text" maxlength=500 name="post_text" aria-label="write blog here" placeholder="what are you thinking about?" required></textarea>
+      <button type="submit">Add Post</button>
+    </form>
+  `
+  );
+}
+
+module.exports = { submitPage };
