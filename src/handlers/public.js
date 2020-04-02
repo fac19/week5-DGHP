81680fbcd03c15b659e42302c3c5715b3f60e2e5
Ivo Evans <ievans.storrie@gmail.com>
2020-04-02 14:56:45 +0100

    Add publicHandler for css and js files Relates #10
    Co-authored-by: tacotoemeck <wagner.tomek@gmail.com>

--- src/handlers/public.js
+++ src/handlers/public.js
@@ -1,1 +1,28 @@
 const templates = require("../template");
+
+const types = {
+  css: "text/css",
+  js: "application/javascript",
+  ico: "image/x-icon"
+};
+
+function publicHandler(request, response) {
+  const url = request.url;
+  const urlArray = url.split(".");
+  const extension = urlArray[1];
+  const type = types[extension];
+
+  const filePath = path.join(__dirname, url);
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(error);
+      response.writeHead(404, { "content-type": type || "text/plain" });
+      response.end("Not found");
+    } else {
+      response.writeHead(200, { "content-type": type });
+      response.end(file);
+    }
+  });
+}
+
+module.exports = publicHandler;
