ecde8e1d90d89a90d38f3fad484849b689a57338
tacotoemeck <wagner.tomek@gmail.com>
2020-04-03 11:30:58 +0100

    Fix src atrribute in template.js missingPage Relates #28

--- src/handlers/submitPost.js
+++ src/handlers/submitPost.js
@@ -1,20 +1,20 @@
 const templates = require("../template");
-const model = require("../model")
+const model = require("../model");
 
 function submitPostHandler(req, res) {
-  let body = ""
-  req.on('data', chunk => body += chunk)
-  req.on('end', () => {
+  let body = "";
+  req.on("data", chunk => (body += chunk));
+  req.on("end", () => {
     const message = new URLSearchParams(body);
     const messageObject = Object.fromEntries(message);
-    console.log(messageObject); 
-    model.newPost(messageObject)
+    model
+      .newPost(messageObject)
       .then(() => {
-        res.writeHead(302, {"location": "/" })
-        res.end()
-      } )
-      .catch(err => console.error(err))
-  } )
+        res.writeHead(302, { location: "/" });
+        res.end();
+      })
+      .catch(err => console.error(err));
+  });
 }
 
-module.exports = submitPostHandler
+module.exports = submitPostHandler;
